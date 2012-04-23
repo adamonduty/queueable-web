68ca421e06eda46fa94310e7cc1495be8b311401
Adam Lamar <adamonduty@gmail.com>
2012-04-23 17:09:45 -0400

    Initial commit of pristine Rails 3.2.3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Queueable::Application.config.secret_token = 'ddaf733af47faeb775524cc2b04b948491f810f2c43e0e83663d9b4c00d72bafc1b54afc135216a40939f80b26cc057830cfadbf21dce8ebe322f2eb92054771'
